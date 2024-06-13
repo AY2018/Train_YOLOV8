afcd9e05d9cecb5a43e5273be0c38e88d3a18c86
Ayoub2024 <ayoub.kahfy@gmail.com>
2024-06-13 14:40:46 +0200

    comments

--- train_yolov8.sh
+++ train_yolov8.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
-#SBATCH --job-name=YoloV8Training
-#SBATCH --output=YoloV8Training%j.out
-#SBATCH --error=YoloV8Training%j.err
-#SBATCH --partition=gpu_p4
-#SBATCH --nodes=1
-#SBATCH --ntasks=8
-#SBATCH --gres=gpu:8
-#SBATCH --cpus-per-task=8
-#SBATCH --time=01:00:00
-#SBATCH --qos=qos_gpu-t4
-#SBATCH --account=your_account_name
+#SBATCH --job-name=YoloV8Training  # Nom du job
+#SBATCH --output=YoloV8Training%j.out  # Fichier de sortie (%j est remplacé par l'ID du job)
+#SBATCH --error=YoloV8Training%j.err  # Fichier d'erreur (%j est remplacé par l'ID du job)
+#SBATCH --partition=gpu_p4  # Partition spécifiée pour utiliser les GPU A100
+#SBATCH --nodes=1  # Réserver un nœud
+#SBATCH --ntasks=8  # Réserver 8 tâches (ou processus)
+#SBATCH --gres=gpu:8  # Réserver 8 GPU
+#SBATCH --cpus-per-task=8  # Réserver 8 CPU par tâche (et mémoire associée)
+#SBATCH --time=01:00:00  # Temps maximal d'allocation (HH:MM:SS)
+#SBATCH --qos=qos_gpu-t4  # Qualité de service (QoS) pour la réservation
+#SBATCH --account=your_account_name  # Nom du compte pour la comptabilité
 
-# Load necessary modules
-module purge
-module load cpuarch/amd
-module load pytorch-gpu/py3/1.12.1
+module purge  # Nettoyer les modules chargés par défaut
 
-# Create and activate the conda environment
-conda create -n yolov8-env python=3.8 -y
-source activate yolov8-env
+module load cpuarch/amd  # Charger les optimisations pour les CPU AMD
 
-# Install YOLOv8 dependencies
-pip install ultralytics
+module load pytorch-gpu/py3/1.12.1  # Charger PyTorch avec support GPU
 
-# Run your YOLOv8 training script
-srun python -u /chemin/train.py
+# Créer et activer un environnement conda
+conda create -n yolov8-env python=3.8 -y  Python 3.8
+source activate yolov8-env  
+
+# Installer les dépendances de YOLOv8
+pip install ultralytics  
+
+# Exécuter votre script de formation YOLOv8
+srun python -u /path/to/your/work/directory/train.py  
